a0f04ba1bd8eac4b13434b483f93c38bc9dbed2b
solarenergy990 <74872776+solarenergy990@users.noreply.github.com>
2021-09-03 08:25:14 +0200

    fixes localStorage bug

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,8 +1,14 @@
 import { useState, useEffect } from 'react';
 
 const useLocalStorage = (key, defaultValue) => {
+  // console.log(defaultValue);
+  // console.log(key);
+  // console.log(window.localStorage.getItem(key));
+
   const [state, setState] = useState(() => {
-    return JSON.parse(window.localStorage.getItem(key) ?? defaultValue);
+    if (window.localStorage.length > 0) {
+      return JSON.parse(window.localStorage.getItem(key));
+    } else return defaultValue;
   });
 
   useEffect(() => {
