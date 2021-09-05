f032889bbe8578b8a1d341469e04b0bedb82fa6a
solarenergy990 <74872776+solarenergy990@users.noreply.github.com>
2021-09-05 17:00:33 +0200

    changes local storage to redux persistor

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,70 +2,43 @@ import React from 'react';
 import ContactForm from '../ContactForm/ContactForm';
 import ContactsList from '../ContactsList/ContactsList';
 import Filter from '../Filter/Filter';
-// import initialContacts from '../../contacts.json';
-// import shortid from 'shortid';
-import { connect } from 'react-redux';
+
+// import { connect } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux';
 import appActions from '../../redux/app/actions';
 
-// import useLocalStorage from '../../hooks/useLocalStorage';
+const App = () =>
+  // {
+  //   // contacts,
+  //   // filter,
+  //   // onHandleSubmit,
+  //   // onDeleteContact,
+  //   // onChangeFilter,
+  // },
+  {
+    const contacts = useSelector(state => state.appState.contacts);
+    const filter = useSelector(state => state.appState.filter);
+    const dispatch = useDispatch();
 
-const App = ({
-  contacts,
-  filter,
-  onHandleSubmit,
-  deleteContact,
-  changeFilter,
-}) => {
-  // const dispatch = useDispatch();
-  // const [contacts, setContacts] = useLocalStorage('contacts', initialContacts);
-  // const [filter, setFilter] = useState('');
+    const onHandleSubmit = contact => dispatch(appActions.addContact(contact));
+    const onDeleteContact = contactId =>
+      dispatch(appActions.deleteContact(contactId));
+    const onChangeFilter = value => dispatch(appActions.setFilter(value));
 
-  // const addContact = data => {
-  //   const { name, number } = data;
+    return (
+      <div>
+        <h2>Phonebook</h2>
+        <ContactForm onSubmit={onHandleSubmit} />
 
-  //   const contact = {
-  //     id: shortid.generate(),
-  //     name,
-  //     number,
-  //   };
-
-  //   const checkedContactNames = contacts.map(contact => {
-  //     return contact.name.toLowerCase();
-  //   });
-
-  //   if (!checkedContactNames.includes(name.toLowerCase())) {
-  //     setContacts(prevContacts => {
-  //       return [...prevContacts, contact];
-  //     });
-  //   } else {
-  //     alert(`${name} is already in contacts`);
-  //   }
-  // };
-
-  // const deleteContact = contactId => {
-  //   setContacts(contacts.filter(contact => contact.id !== contactId));
-  // };
-
-  // const changeFilter = evt => {
-  //   setFilter(evt.currentTarget.value);
-  // };
-
-  // const visibleContacts = getVisibleContacts();
-
-  return (
-    <div>
-      <h2>Phonebook</h2>
-      <ContactForm onSubmit={value => onHandleSubmit(value)} />
-
-      <h2>Contacts</h2>
-      <Filter value={filter} onChange={changeFilter} />
-      <ContactsList
-        contacts={getVisibleContacts(contacts, filter)}
-        onContactDelete={deleteContact}
-      />
-    </div>
-  );
-};
+        <h2>Contacts</h2>
+        <Filter value={filter} onChange={onChangeFilter} />
+        <ContactsList
+          contacts={getVisibleContacts(contacts, filter)}
+          onContactDelete={onDeleteContact}
+        />
+      </div>
+    );
+  };
 
 const getVisibleContacts = (contacts, filter) => {
   if (!filter) {
@@ -79,18 +52,17 @@ const getVisibleContacts = (contacts, filter) => {
   );
 };
 
-const mapStateToProps = state => {
-  // console.log(state);
-  return {
-    contacts: state.appState.contacts,
-    filter: state.appState.filter,
-  };
-};
+// const mapStateToProps = state => {
+//   return {
+//     contacts: state.appState.contacts,
+//     filter: state.appState.filter,
+//   };
+// };
 
-const mapDispatchToProps = dispatch => ({
-  onHandleSubmit: value => dispatch(appActions.addContact(value)),
-  deleteContact: value => dispatch(appActions.deleteContact(value)),
-  changeFilter: value => dispatch(appActions.setFilter(value)),
-});
+// const mapDispatchToProps = dispatch => ({
+//   onHandleSubmit: value => dispatch(appActions.addContact(value)),
+//   onDeleteContact: value => dispatch(appActions.deleteContact(value)),
+//   onChangeFilter: value => dispatch(appActions.setFilter(value)),
+// });
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,41 @@
-import { createStore, combineReducers } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+// import { createStore, combineReducers } from 'redux';
+// import { composeWithDevTools } from 'redux-devtools-extension';
+import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import appReducer from './app/reducer';
 
-const rootReducer = combineReducers({
-  appState: appReducer,
+const persistConfig = {
+  key: 'appState',
+  storage,
+  blacklist: ['filter'],
+};
+
+// const rootReducer = combineReducers({
+//   appState: appReducer,
+// });
+
+const store = configureStore({
+  reducer: { appState: persistReducer(persistConfig, appReducer) },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
-const store = createStore(rootReducer, composeWithDevTools());
+// const store = createStore(persistedReducer, composeWithDevTools());
 
-export default store;
+const persistor = persistStore(store);
+
+export default { store, persistor };
